77225e068481d2a798e24745865ffa96f0f4e2e4
Sdsswt <terkeurswillem@gmail.com>
2024-10-22 11:36:49 -0700

    done

--- a7.py
+++ a7.py
@@ -11,3 +11,8 @@ Read through the file **example1.py** for information on using the math module.
 * You will require the use of the round() function from example1.py
 '''
 
+f = float(62.5)
+c = float()
+
+c = (f-32)* 5/9
+print(round(c,1))
--- a8.py
+++ a8.py
@@ -14,3 +14,13 @@ You may use either the ** operator or math.pow(x,y) for your exponents
 You may use either math.sqrt(x) or the exponent to the power of 0.5 for your square root
 
  '''
+
+a = float(5)
+b = float(8)
+c = float()
+
+
+c = a**2 + b**2
+c= c**0.5
+
+print(c)
